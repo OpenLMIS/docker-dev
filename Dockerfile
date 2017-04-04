3a0696658f7c2273c088c674de5f84ad322249d6
Paweł Nawrocki <pnawrocki@soldevelo.com>
2017-04-04 14:30:45 +0200

    Move sync_transifex into base dev image

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM anapsix/alpine-java:jdk8
 
 WORKDIR /root
-COPY installGradle.sh .
+
+COPY install_gradle.sh .
 ENV GRADLE_USER_HOME /gradle
 ENV GRADLE_OPTS -Dorg.gradle.daemon=true
+
 RUN apk update && \
   apk add libstdc++ && \
   apk add bash && \
@@ -14,10 +16,12 @@ RUN apk update && \
   pip install transifex-client && \
   mkdir bin && \
   npm install -g raml-cop && \
-  /bin/sh installGradle.sh
+  /bin/sh install_gradle.sh
 
 WORKDIR /app
 
+COPY sync_transifex.sh /transifex/sync_transifex.sh
 VOLUME ["/app", "/gradle"]
+
 EXPOSE 8080
 CMD ["bash"]
---
+++ sync_transifex.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# This script initializes the Transifex client. It then uploads the source resource to the 
+# Transifex project and downloads the translated resources into the build.
+
+# Some things to note:
+# - The .tx folder, if it exists, is deleted beforehand so that the 'tx init' does not need to 
+#   prompt to overwrite it.
+# - The .tx folder is generated each time, rather than saving the .tx/config file into version 
+#   control, because a .transifexrc also needs to be generated using the username and password.
+#   Since these credentials should not be in version control, the regeneration approach is used.
+
+rm -rf .tx
+
+# Set default behavior based on environment variables (pull by default, push only on purpose)
+TX_PUSH=${TRANSIFEX_PUSH:-false}
+TX_PULL=${TRANSIFEX_PULL:-true}
+
+while [[ $# -gt 1 ]]; do
+  case "$1" in
+    --push)
+      TX_PUSH=true ;;
+    --pull)
+      TX_PULL=true ;;
+    --source-file)
+      SOURCE_FILE="$2"
+      shift
+      ;;
+    --resource)
+      RESOURCE="$2"
+      ;;
+    --pattern)
+      PATTERN="$2"
+      ;;
+    *)
+      ;;
+  esac
+  shift
+done
+
+tx init --host=https://www.transifex.com --user=$TRANSIFEX_USER --pass=$TRANSIFEX_PASSWORD
+
+tx set --auto-local -r $RESOURCE \
+  $PATTERN --source-lang en --type UNICODEPROPERTIES \
+  --source-file $SOURCE_FILE --execute
+
+if [ "$TX_PUSH" == "true" ]; then
+  tx push -s
+fi
+
+if [ "$TX_PULL" == "true" ]; then
+  tx pull -a -f
+fi
