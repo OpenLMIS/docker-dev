672de040536f2a9d174195b1baaffe16862d2e31
hdsldv <88383112+hdsldv@users.noreply.github.com>
2022-04-06 14:59:45 +0200

    OLMIS-7568 Change gradle to v5.2.1 for image v8

--- install_gradle.sh
+++ install_gradle.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -xe
 
-# SHA256 of gradle-4.10.2-bin.zip
-CHECKSUM=b49c6da1b2cb67a0caf6c7480630b51c70a11ca2016ff2f555eaeda863143a29
+# SHA256 of gradle-5.2.1-bin.zip
+CHECKSUM=748c33ff8d216736723be4037085b8dc342c6a0f309081acf682c9803e407357
 
-wget -O gradle.zip 'https://downloads.gradle-dn.com/distributions/gradle-4.10.2-bin.zip'
+wget -O gradle.zip 'https://downloads.gradle-dn.com/distributions/gradle-5.2.1-bin.zip'
 echo "$CHECKSUM  gradle.zip" | sha256sum -c -w -
 
 unzip gradle.zip
 rm gradle.zip
-ln -s "$HOME/gradle-4.10.2/bin/gradle" /sbin/gradle
+ln -s "$HOME/gradle-5.2.1/bin/gradle" /sbin/gradle
