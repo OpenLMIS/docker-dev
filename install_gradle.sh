d2357a6ded90aaa688c5b9d87fa65660b29b88a7
Piotr Wargulak <pwargulak@soldevelo.com>
2025-05-07 12:11:43 +0200

    OLMIS-8156: Fixed comment

--- install_gradle.sh
+++ install_gradle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -xe
 
-# SHA256 of gradle-5.2.1-bin.zip
+# SHA256 of gradle-6.8.3-bin.zip
 CHECKSUM=7faa7198769f872826c8ef4f1450f839ec27f0b4d5d1e51bade63667cbccd205
 
 wget -O gradle.zip 'https://services.gradle.org/distributions/gradle-6.8.3-bin.zip'
